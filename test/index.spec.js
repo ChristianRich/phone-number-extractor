6efbb145624953136fb4e7b7bae6a4d7847ddd41
unknown <christian.rich@SYD-NB5586.cnet.cnwk>
2015-09-28 14:26:08 +1000

    Rewrite rules

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,14 +4,118 @@ var assert = require('assert')
 	, Extractor = require('../lib/Extractor')
 	, mockData = require('./mockdata/au');
 
-describe('Phone number extractor', function() {
+describe('Australia', function() {
 
-    it('should extract phone number', function (done) {
+    it('should extract 1 mobile number (real text)', function (done) {
 
         var extractor = new Extractor(mockData.realText1, 'au');
 
         extractor.run(function(err, res){
-            assert(res[0] === '0434100200', 'Should be 0434 100200');
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 1, 'Result length should be 1');
+            done();
+        });
+    });
+
+    it('should extract 1 mobile number and 1 landline (real text)', function (done) {
+
+        var extractor = new Extractor(mockData.realText2, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 2, 'Result length should be 2');
+            assert(res[0] === '0266771102', 'Should be 0266771102');
+            assert(res[1] === '0422150394', 'Should be 0422150394');
+            done();
+        });
+    });
+
+    it('should not extract any numbers (real text)', function (done) {
+
+        var extractor = new Extractor(mockData.realText3, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 0, 'Result length should be 0');
+            done();
+        });
+    });
+
+    it('should  extract 1 number (real text)', function (done) {
+
+        var extractor = new Extractor(mockData.realText4, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 1, 'Result length should be 1');
+            done();
+        });
+    });
+
+    it('should extract 2 obfuscated mobile numbers', function (done) {
+
+        var extractor = new Extractor(mockData.obfuscated, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 2, 'Result length should be 2');
+            assert(res[0] === '0410542543', 'Should be 0410542543');
+            assert(res[1] === '0410541451', 'Should be 0410541451');
+            done();
+        });
+    });
+
+    it('should extract 14 mobile numbers', function (done) {
+
+        var extractor = new Extractor(mockData.mobile, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 14, 'Result length should be 64');
+            done();
+        });
+    });
+
+    it('should extract 64 mobile numbers with an international prefix', function (done) {
+
+        var extractor = new Extractor(mockData.mobileInternationalPrefix, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 52, 'Result length should be 64');
+            done();
+        });
+    });
+
+    it('should extract 16 landline numbers', function (done) {
+
+        var extractor = new Extractor(mockData.landline, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 16, 'Result length should be 64');
+            done();
+        });
+    });
+
+    it('should extract 64 landline numbers with an international prefix', function (done) {
+
+        var extractor = new Extractor(mockData.landlineInternationalPrefix, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 64, 'Result length should be 64');
+            done();
+        });
+    });
+
+    it('should not extract invalid numbers / false positives', function (done) {
+
+        var extractor = new Extractor(mockData.invalid, 'au');
+
+        extractor.run(function(err, res){
+            assert(_.isArray(res), 'Result should be type Array');
+            assert(res.length === 0, 'Result length should 0');
             done();
         });
     });
--- test/mockdata/au.js
+++ test/mockdata/au.js
@@ -1,40 +1,184 @@
-var realText1 = "This awesome looking road bike, is Look’s top of the range 695 SR superlight carbon frame built up with America’s flagship Sram Red 10speed group set with Look carbon cranks and a choice of Mavic Ksyrium Elite low profile rims, or Zipp 404 full carbon rims! Advertised price is with the Mavic rims which come with a brand new Sram Red cassette and as new Continental Ultra Race tires. The seat is a Fizik’s saddle and the bars are Zipp Service Course SL’s. Comes with a Look bag with all the original paper work, spacers, tools etc. This is a superlight weight no nonsense race bike (the SR stands for Super Rigid) hand built in France! The frame size is 56 cm (Integrated seat post is adjustable!) and suitable for riders of 175 to 185cm or 5ft9 to 6ft1. Enquire on 0434 100200 after 6pm. Thanks.";
+module.exports.realText1 = 'This awesome looking road bike, is Look’s top of the range 695 SR superlight carbon frame built up with America’s flagship Sram Red 10speed group set with Look carbon cranks and a choice of Mavic Ksyrium Elite low profile rims, or Zipp 404 full carbon rims! Advertised price is with the Mavic rims which come with a brand new Sram Red cassette and as new Continental Ultra Race tires. The seat is a Fizik’s saddle and the bars are Zipp Service Course SL’s. Comes with a Look bag with all the original paper work, spacers, tools etc. This is a superlight weight no nonsense race bike (the SR stands for Super Rigid) hand built in France! The frame size is 56 cm (Integrated seat post is adjustable!) and suitable for riders of 175 to 185cm or 5ft9 to 6ft1. Enquire on 0434 100200 after 6pm. Thanks.';
+module.exports.realText2 = 'niaaan elgrand , rider , this is a very cool van , it is low and quick ... i bought it for my son , but he bought another car instead !! V6 NISSAN super reliable bulletproof motor ... it is close to ready for rego , can be driven home on a permit , needs a small weld on the muffler , also seat covers ( there is a small tear in the passenger leather seat ), there is a very slight oil leak in the rocker cover gasket too , hardly noticable but it gets on the manifold and makes a slight fume (smell) . the front bar has been repaired (fibreglass) paint ready ... the suspension has is custom adjustable coil over shocks , well worth an inspection if you want this type of van ... air is cold , stereo sounds great , it has factory carpet covers , even the electric curtains are working !! not much to do to get a cool ride ... at $4900 it is a bargain ... please call 0266771102 ... 0422150394 or email me , offers considered , i can send more pics too .';
+module.exports.realText3 = "270 RWKW professionally tuned by DPM Fitted with a Di-Fillipo 2.5' full exhaust system including extractors, air intake and Maf sensor sounds and drives great. Driven by company director as daily driver only. It has just had full service so nothing due until 90,000 Klms. This Holden Commodore SS V Sport wagon (Wagon) comes with full service history from Holden dealer and a roadworthy certificate. It won't last long at this price $35,000. 76534 km on the clock only. This Holden Commodore 2011 SS V has iPod connectivity, bluetooth and USB audio input. It is all about family when they're in it, and about you when they're not. It has 6 airbags fitted for your protection. This car has rear parking sensors, front fog lamps and cruise control. This car is priced to sell quickly at $35,000.";
+module.exports.realText4 = "Air Conditioned, Power Steering, SUPER DOOPER LOW KLM's @ 78,238, SET AND FORGET REGO Until June 2016!!, Power Mirrors, Tinted Windows, Central Locking, CD Mp3/AUX/USB AM/FM Stereo, Bluetooth Connectivity, Partial Leather Interior, Dual SRS Air Bags, In Cabin Roll Bar, Rear Tow Bar Accessory, EFS Lift Kit Upgrade, Side Steps,  Added Essential Upgrades: - Shovel - Farm Jack - Sand Ladder - CB Radio (Oricom) - Brand New Mud Tyres with Sunraysia Rims - Dual Front ARB LED Spot Lights (2 x 185W) - Front Bull Bar - Full Length Top Luggage Rack - Fire Extinguisher - Rear Cabin Cage - Genuine Snorkel - Fuel Cans A STEAL at This Price! What a GEM! This Is a Must See!!! Immaculate Condition Inside & Out, Nothing To Spend!!!  Enquire Today!! DO NOT MISS OUT! We offer: *5 Year Unlimited Klms Warranty Plus 24/7 Roadside Service Australia Wide (terms & conditions apply) *100% clear title includes -No Accident History (no written off) -No Encumbrance Owing (no money owing) *Trades-Ins & Test Drive Available *Extended Trading Hours: Open 7 Days A Week: -Mon-Fri 9am - 5:30 pm -Sat 9am- 5pm -Sun 10am - 4pm (after hour appointments available) *Contact Us For On 0254 123 123 + click to reveal *Website: http://www.stevesautoworld.com.au *Find Us On Facebook & Like Our Page, https://www.facebook.com/steves.autoworld";
 
-var mobileList =
-    "call me 0434100000" +
-    "call me 0434 100100" +
-    "call me 0434 100 100" +
-    "call me 0434 10 01 00" +
-    "call me 04 34100200" +
-    "call me 04 34 100200" +
-    "call me 04 34 100 200" +
-    "call me 04 34 10 02 00" +
-    "call me 043 4100300" +
-    "call me 043 410 0300" +
-    "call me 043 4100 300" +
-    "call me 043 41 00 300" +
-    "call me 043 41 00 30 0";
+module.exports.mobile =
+    'call me 0434100000' +
+    'call me 0434 100101' +
+    'call me 0434 100 102' +
+    'call me 0434 10 01 03' +
+    'call me 04 34100204' +
+    'call me 04 34 100205' +
+    'call me 04 34 100 206' +
+    'call me 04 34 10 02 07' +
+    'call me 043 4100308' +
+    'call me 043 410 0309' +
+    'call me 043 4100 310' +
+    'call me 043 41 00 311' +
+    'call me 043 41 00 31 2' +
+    'call me 0 4 3 4 1 0 0 3 1 2';
 
-var landLineList =
-    "call me 0234100401" +
-    "call me 02 34100402" +
-    "call me 02 3410 0403" +
-    "call me 02 341 004 04" +
-    "call me 0334100401" +
-    "call me 03 34100402" +
-    "call me 03 3410 0403" +
-    "call me 03 341 004 04" +
-    "call me 0734100401" +
-    "call me 07 34100402" +
-    "call me 07 3410 0403" +
-    "call me 07 341 004 04" +
-    "call me 0834100401" +
-    "call me 08 34100402" +
-    "call me 08 3410 0403" +
-    "call me 08 341 004 04" ;
+module.exports.obfuscated =
+    'I am an obfuscated phone number ***;*04;10.542.543;; and I do not wish to be found' +
+    'I am also someone trying to hide my number I am 5 feel tall drive a 2006 Toyota try and call **04** 10...541.451';
 
-module.exports = {
-    realText1: realText1,
-    mobileList: mobileList,
-    landLineList: landLineList
-};
+module.exports.mobileInternationalPrefix =
+
+    // +61
+    'call me +61 434100000' +
+    'call me +61 434 100101' +
+    'call me +61 434 100 102' +
+    'call me +61 434 10 01 03' +
+    'call me +61 4 34100204' +
+    'call me +61 4 34 100205' +
+    'call me +61 4 34 100 206' +
+    'call me +61 4 34 10 02 07' +
+    'call me +61 43 4100308' +
+    'call me +61 43 410 0309' +
+    'call me +61 43 4100 310' +
+    'call me +61 43 41 00 311' +
+    'call me +61 43 41 00 31 2' +
+
+    // +61 no space
+    'call me +61434100000' +
+    'call me +61434 100101' +
+    'call me +61434 100 102' +
+    'call me +61434 10 01 03' +
+    'call me +614 34100204' +
+    'call me +614 34 100205' +
+    'call me +614 34 100 206' +
+    'call me +614 34 10 02 07' +
+    'call me +6143 4100308' +
+    'call me +6143 410 0309' +
+    'call me +6143 4100 310' +
+    'call me +6143 41 00 311' +
+    'call me +6143 41 00 31 2' +
+
+    // 0061
+    'call me 0061 434 111 111' +
+    'call me 0061 434 222 222' +
+    'call me 0061 434 333 333' +
+    'call me 0061 434 10 01 03' +
+    'call me 0061 4 34100204' +
+    'call me 0061 4 34 100205' +
+    'call me 0061 4 34 100 206' +
+    'call me 0061 4 34 10 02 07' +
+    'call me 0061 43 4100308' +
+    'call me 0061 43 410 0309' +
+    'call me 0061 43 4100 310' +
+    'call me 0061 43 41 00 311' +
+    'call me 0061 43 41 00 31 2' +
+
+    // 0061 no space
+    'call me 0061434100000' +
+    'call me 0061434 100101' +
+    'call me 0061434 100 102' +
+    'call me 0061434 10 01 03' +
+    'call me 00614 34100204' +
+    'call me 00614 34 100205' +
+    'call me 00614 34 100 206' +
+    'call me 00614 34 10 02 07' +
+    'call me 006143 4100308' +
+    'call me 006143 410 0309' +
+    'call me 006143 4100 310' +
+    'call me 006143 41 00 311' +
+    'call me 006143 41 00 31 2';
+
+module.exports.landline =
+    'call me 0234100401' +
+    'call me 02 34100402' +
+    'call me 02 3410 0403' +
+    'call me 02 341 004 04' +
+    'call me 0334100401' +
+    'call me 03 34100402' +
+    'call me 03 3410 0403' +
+    'call me 03 341 004 04' +
+    'call me 0734100401' +
+    'call me 07 34100402' +
+    'call me 07 3410 0403' +
+    'call me 07 341 004 04' +
+    'call me 0834100401' +
+    'call me 08 34100402' +
+    'call me 08 3410 0403' +
+    'call me 08 341 004 04' ;
+
+module.exports.landlineInternationalPrefix =
+
+    // +61
+    'call me +61 234100401' +
+    'call me +61 2 34100402' +
+    'call me +61 2 3410 0403' +
+    'call me +61 2 341 004 04' +
+    'call me +61 334100401' +
+    'call me +61 3 34100402' +
+    'call me +61 3 3410 0403' +
+    'call me +61 3 341 004 04' +
+    'call me +61 734100401' +
+    'call me +61 7 34100402' +
+    'call me +61 7 3410 0403' +
+    'call me +61 7 341 004 04' +
+    'call me +61 834100401' +
+    'call me +61 8 34100402' +
+    'call me +61 8 3410 0403' +
+    'call me +61 8 341 004 04' +
+
+    // +61 no space
+    'call me +61234100401' +
+    'call me +612 34100402' +
+    'call me +612 3410 0403' +
+    'call me +612 341 004 04' +
+    'call me +61334100401' +
+    'call me +613 34100402' +
+    'call me +613 3410 0403' +
+    'call me +613 341 004 04' +
+    'call me +61734100401' +
+    'call me +617 34100402' +
+    'call me +617 3410 0403' +
+    'call me +617 341 004 04' +
+    'call me +61834100401' +
+    'call me +618 34100402' +
+    'call me +618 3410 0403' +
+    'call me +618 341 004 04' +
+
+    // 0061
+    'call me 0061 234100401' +
+    'call me 0061 2 34100402' +
+    'call me 0061 2 3410 0403' +
+    'call me 0061 2 341 004 04' +
+    'call me 0061 334100401' +
+    'call me 0061 3 34100402' +
+    'call me 0061 3 3410 0403' +
+    'call me 0061 3 341 004 04' +
+    'call me 0061 734100401' +
+    'call me 0061 7 34100402' +
+    'call me 0061 7 3410 0403' +
+    'call me 0061 7 341 004 04' +
+    'call me 0061 834100401' +
+    'call me 0061 8 34100402' +
+    'call me 0061 8 3410 0403' +
+    'call me 0061 8 341 004 04' +
+
+    // 0061 no space
+    'call me 0061234100401' +
+    'call me 00612 34100402' +
+    'call me 00612 3410 0403' +
+    'call me 00612 341 004 04' +
+    'call me 0061334100401' +
+    'call me 00613 34100402' +
+    'call me 00613 3410 0403' +
+    'call me 00613 341 004 04' +
+    'call me 0061734100401' +
+    'call me 00617 34100402' +
+    'call me 00617 3410 0403' +
+    'call me 00617 341 004 04' +
+    'call me 0061834100401' +
+    'call me 00618 34100402' +
+    'call me 00618 3410 0403' +
+    'call me 00618 341 004 04';
+
+module.exports.invalid =
+    'call me 434 800 988' +
+    'call me 1434 800 988' +
+    'call me 0123 800 988' +
+    'call me 0534 800 988';
